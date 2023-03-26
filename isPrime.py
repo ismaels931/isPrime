977b896c8af0fca340cc500980cbf489fd60772e
Lucas Varela Correa <98191834+SRW-lucap@users.noreply.github.com>
2023-03-26 17:45:52 +0200

    Update isPrime.py

--- isPrime.py
+++ isPrime.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python
+def isPrime(number : int) -> bool:
+    if number <= 1:
+        return False
 
-def isPrime(n):
-    if n <= 1: return False
-    i = 2
-    while i**2 <= n:
-        if n % i == 0:
+    counter = 2
+
+    while counter ** 2 <= number:
+        if number % counter == 0:
             return False
-        i += 1
+
+        counter += 1
+
     return True
 
-def main():
-    n = int(input('Enter a number: '))
-    count = 0
-    for i in range(1, n + 1):
-        if isPrime(i): print i,
-
 if __name__ == '__main__':
-    main()
+    numbers = int(input('Enter a number => '))
+
+    for number in range(1, numbers + 1):
+        if isPrime(number):
+            print(number)
